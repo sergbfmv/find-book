b2cdcb4af3c11bbcb4e6665c00c704712073c6aa
Sergey <sergbfmv@mail.ru>
2024-01-16 23:02:53 +0300

    fix some bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/book-landing-page/',
+  base: '/find-book/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
